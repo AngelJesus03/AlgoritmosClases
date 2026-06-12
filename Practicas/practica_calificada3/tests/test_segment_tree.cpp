2af2fa6e1dc9c2bad9410e2ddc3cddd638eb79a5
AngelJesus <angel.aj.naru@gmail.com>
2026-06-12 06:40:57 -0500

    tests final

---
+++ Practicas/practica_calificada3/tests/test_segment_tree.cpp
@@ -0,0 +1,76 @@
+#include <cassert>
+#include <iostream>
+#include "../include/solution.h"
+using namespace std;
+
+void ejecutar_test(int x[], int n, int p[], int esperado[], const string& nombre){
+    ods::SegmentTree st;
+    st.n = n;
+    st.build(1, 1, n);
+    for(int i = 0; i < n; i++){
+        int idx = st.kesimo(1, 1, n, p[i]);
+        assert(x[idx] == esperado[i]);
+        st.remove(1, 1, n, idx);
+    }
+    cout << nombre << " paso" << endl;
+}
+
+void comparar_con_ingenua(int x[], int n, int p[], const string& nombre){
+    int res_st[200005];
+    ods::SegmentTree st;
+    st.n = n;
+    st.build(1, 1, n);
+    for(int i = 0; i < n; i++){
+        int idx = st.kesimo(1, 1, n, p[i]);
+        res_st[i] = x[idx];
+        st.remove(1, 1, n, idx);
+    }
+    int res_li[200005];
+    ods::ListaIngenua li;
+    li.build(x, n);
+    for(int i = 0; i < n; i++)
+        res_li[i] = li.remove(p[i]);
+    for(int i = 0; i < n; i++)
+        assert(res_st[i] == res_li[i]);
+    cout << nombre << " paso" << endl;
+}
+
+void test_invariante(const string& nombre){
+    ods::SegmentTree st;
+    st.n = 5;
+    st.build(1, 1, 5);
+    for(int i = 0; i < 5; i++){
+        assert(st.tree[1] == 5 - i);
+        st.remove(1, 1, 5, st.kesimo(1, 1, 5, 1));
+    }
+    assert(st.tree[1] == 0);
+    cout << nombre << " paso" << endl;
+}
+
+int main(){
+    //caso 1: ejemplo del problema
+    int x1[] = {0, 2, 6, 1, 4, 2};
+    int p1[] = {3, 1, 3, 1, 1};
+    int e1[] = {1, 2, 2, 6, 4};
+
+    //caso 2: eliminar siempre el primero
+    int x2[] = {0, 1, 2, 3, 4, 5};
+    int p2[] = {1, 1, 1, 1, 1};
+    int e2[] = {1, 2, 3, 4, 5};
+
+    //caso 3: eliminar siempre el ultimo
+    int x3[] = {0, 1, 2, 3, 4, 5};
+    int p3[] = {5, 4, 3, 2, 1};
+    int e3[] = {5, 4, 3, 2, 1};
+
+
+    cout << "Ejecutando los tests con SegmentTree (solucion principal)\n" << endl;
+
+    ejecutar_test(x1, 5, p1, e1, "Caso de ejemplo del problema");
+    ejecutar_test(x2, 5, p2, e2, "Caso de eliminar siempre el primero");
+    ejecutar_test(x3, 5, p3, e3, "Caso de eliminar siempre el ultimo");
+    test_invariante("Caso de invariante principal");
+
+    cout << "\nTodos los tests pasaron" << endl;
+    return 0;
+}
